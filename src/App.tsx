ab8173124f4c39daa85206f5a72e60c237e622b7
Sina-Sche <sina.scheckenbach@sinnerschrader.com>
2022-05-05 17:09:53 +0200

    feat: adds connection to contentful

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { client } from "./client";
 
 function App() {
+  const [data, setData] = useState<any>();
+  const entryId = process.env.REACT_APP_ENTRY_ID;
+  useEffect(() => {
+    if (entryId) {
+      client
+        .getEntry(entryId)
+        .then((response) => {
+          setData(response.fields);
+        })
+        .catch((e) => console.error(e));
+    }
+  }, [entryId]);
+  data &&
+    console.log("Description", data.description.content[0].content[0].value);
   return (
     <div className="App">
       <h1>Contentful Test</h1>
+      {data && (
+        <>
+          <img
+            src={data.image.fields.file.url}
+            alt={data.image.fields.description}
+          />
+          <span>{data.headline}</span>
+          <section>{data.description.content[0].content[0].value}</section>
+          <section>{data.recipe}</section>
+        </>
+      )}
     </div>
   );
 }
